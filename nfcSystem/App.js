11fd88f7df3a5785d916eb1b10265c4cf872eb81
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-06-24 00:06:43 +0530

    NFC Setyp

--- nfcSystem/App.js
+++ nfcSystem/App.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme/index";
 import { HomeScreen } from "./src/features/home/screens/home.screen";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
         <HomeScreen />
       </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
--- nfcSystem/src/features/home/screens/home.screen.js
+++ nfcSystem/src/features/home/screens/home.screen.js
@@ -1,20 +1,38 @@
-import { StatusBar as ExpoStatusBar, StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Text } from "../../../components/typography/text_component";
+import NfcManager, { NfcEvents } from "react-native-nfc-manager";
+import styled from "styled-components/native";
 
 export const HomeScreen = () => {
+  const handleTagDiscovery = async () => {
+    console.log("NFC card detected");
+  };
+  useEffect(() => {
+    NfcManager.start();
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, handleTagDiscovery);
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+      NfcManager.cancelTechnologyRequest().catch(() => {});
+      NfcManager.unregisterTagEvent().catch(() => {});
+    };
+  }, []);
+
   return (
     <>
       <SafeAreaView style={styles.androidSafeArea}>
-        <Text style={{ textAlign: "center" }} variant="label">
-          Hey
-        </Text>
+        <Text>Scan the Tag</Text>
       </SafeAreaView>
     </>
   );
 };
-
 const styles = StyleSheet.create({
   androidSafeArea: {
     flex: 1,
