f66833521b26af053c494c54ad5de2c5ed9c82a4
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-07-05 23:48:33 +0530

    Attendance API

--- nfc_system/lib/providers/AttendanceProvider.dart
+++ nfc_system/lib/providers/AttendanceProvider.dart
@@ -2,17 +2,20 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../models/AttendanceModel.dart';
+import 'package:intl/intl.dart';
 
 class AttendanceProvider with ChangeNotifier {
   List<Attendance> _attendanceItems = [];
   Future<void> createAttendance(Attendance attendance) async {
     final url = "https://mic-attendance-system.onrender.com/mark/attendance/";
     final headers = {'Content-Type': 'application/json'};
+    final dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
+    final formattedDate = dateFormat.format(attendance.date);
     final body = json.encode({
       'meeting_id': attendance.meetingId,
       'user_id': attendance.userId,
       'status': attendance.status,
-      'date': attendance.date.toIso8601String(),
+      'date': formattedDate,
     });
     final response = await http.post(Uri.parse(url), 
     headers: headers,
